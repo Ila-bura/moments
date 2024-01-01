61c267d4ad7ea20d61d2f5e97395b5e66fe340d8
Ila-bura <>
2024-01-01 15:00:43 +0000

    access and refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://api-project-ila-f8d6f876a2c2.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
